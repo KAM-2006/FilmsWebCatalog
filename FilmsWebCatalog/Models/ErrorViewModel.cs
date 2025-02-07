daad7d9edd9d4485ed944be1bc71b2557563d05c
KAM-2006 <dinaoftheday@abv.bg>
2025-02-07 12:58:31 +0200

    Add project files.

---
+++ FilmsWebCatalog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FilmsWebCatalog.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
