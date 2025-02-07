c6c8d0bf118d4dfd598e80c48919afadc4c1718a
KAM-2006 <dinaoftheday@abv.bg>
2025-02-07 14:08:55 +0200

    Created "Film" Entity Class

---
+++ FilmsWebCatalog/Data/Models/Film.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmsWebCatalog.Data.Models
+{
+	public class Film
+	{
+		public Film()
+		{
+
+		}
+		public Film(string title, string filmDirector, string dateOfReleasing,
+			double rating, int directorId, int genreId)
+		{
+			this.Title = title;
+			this.DateOfReleasing = dateOfReleasing;
+			this.Rating = rating;
+			this.GenreID = genreId;
+			this.DirectorID = directorId;	
+		}
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		public string DateOfReleasing { get; set; }
+		public double Rating { get; set; }
+		public int GenreID { get; set; }
+		public Genre Genre { get; set; }
+		public int DirectorID { get; set; }
+		public Director Director { get; set; }
+		//public ICollection<FilmActor> FilmsActors { get; set; }
+
+		[Required]
+		public string UserID { get; set; } = null!;
+		public IdentityUser User { get; set; } = null!;
+
+		public override string ToString()
+		{
+			return $"{this.Title} - {this.DateOfReleasing}, {this.Rating}/10 {this.GenreID} {this.DirectorID}.";
+		}
+	}
+}
