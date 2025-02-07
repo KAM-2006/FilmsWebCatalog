502d58047dac8dbec2136649a4d927952f46be70
Yordanka_2510 <iordanka06@gmail.com>
2025-02-07 16:05:39 +0200

    Created View/Genre/Index for Genre

---
+++ FilmsWebCatalog/Controllers/GenreController.cs
@@ -0,0 +1,19 @@
+﻿using FilmsWebCatalog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmsWebCatalog.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly FilmsWebCatalogAppDbContext context;
+        public GenreController(FilmsWebCatalogAppDbContext _context)
+        {
+            this.context = _context;
+        }
+        public IActionResult Index()
+        {
+            var genres = context.Genres.OrderByDescending(x=>x.Id).ToList();
+            return View(genres);
+        }
+    }
+}
