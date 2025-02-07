0bf03078d6d8d8f4a1ba33f7224b3ef6fa705851
Yordanka_2510 <iordanka06@gmail.com>
2025-02-07 14:00:46 +0200

    Created "Genre" Entity Class

---
+++ FilmsWebCatalog/Data/Models/Genre.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmsWebCatalog.Data.Models
+{
+	public class Genre
+	{
+		public Genre()
+		{
+
+		}
+		public Genre(string name)
+		{
+			this.Name = name;
+		}
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		//public ICollection<Film> Films { get; set; }
+	}
+}
