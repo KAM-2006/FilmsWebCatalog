6f67df5d550567f474d01180f9a4985c1ca102ee
RaliSs6 <ralissprogram2006@gmail.com>
2025-02-07 14:03:56 +0200

    Created "Director" Entity Class

---
+++ FilmsWebCatalog/Data/Models/Director.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmsWebCatalog.Data.Models
+{
+	public class Director
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+		public string DateOfBirth { get; set; }
+		//public ICollection<Film> Films { get; set; }
+	}
+}
