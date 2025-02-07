57cf197a76ad4caba9d930d75426f76ba7a0eece
KAM-2006 <dinaoftheday@abv.bg>
2025-02-07 14:06:27 +0200

    Created "Actor" Entity Class

---
+++ FilmsWebCatalog/Data/Models/Actor.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmsWebCatalog.Data.Models
+{
+	public class Actor
+	{
+		public Actor()
+		{
+
+		}
+		public Actor(string firstName, string lastName, int years, string DOB)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Years = years;
+			this.DateOfBirth = DOB;
+		}
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		public int Years { get; set; }
+		public string DateOfBirth { get; set; }
+		//public ICollection<FilmActor> FilmsActors { get; set; }
+
+		public override string ToString()
+		{
+			return $"{this.FirstName} {this.LastName} {this.Years} {this.DateOfBirth}";
+		}
+	}
+}
